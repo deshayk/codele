0acc7464ae90bb9b6808e8d09ae419d17ce15c53
deshayk <deshaykidd@gmail.com>
2022-05-17 23:38:06 -0500

    add comment

---
+++ main.py
@@ -0,0 +1,6 @@
+'''
+This module is where the main program will run. It will first call the welcome function,
+where the user will be greeted and asked to choose a difficulty level. Then, it will call
+the function that will generate a random prompt from the prompts array based on the user's
+chosen difficulty level.
+'''
