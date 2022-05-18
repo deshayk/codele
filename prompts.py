2d6da3675ee2bc0f8e618bc07fd95bf110a0aca1
deshayk <deshaykidd@gmail.com>
2022-05-17 23:32:28 -0500

    initial commit

---
+++ prompts.py
@@ -0,0 +1,11 @@
+'''
+This module is a collection of prompts for the user. There are three types of prompts the
+user will be able to choose from: easy, medium, and hard. The prompts are saved in an array
+and will be generated randomly for the user.
+'''
+
+# Easy prompts
+
+# Medium prompts
+
+# Hard prompts
