f7c2da66b605540abe0daa9cbecc4c0e1e654394
deshayk <deshaykidd@gmail.com>
2022-05-18 23:59:57 -0500

    add prompt lists

--- prompts.py
+++ prompts.py
@@ -7,16 +7,11 @@ and will be generated randomly for the user.
 # Easy prompts
 
 easy_prompts = [
-  '''Given an array of size n, find the majority element. The majority element is the element that appears more than floor(n/2) times.
-You may assume that the array is non-empty and the majority element always exist in the array.
 
-Example :
-
-Input : [2, 1, 2]
-Return  : 2 which occurs 2 times which is greater than 3/2.''',
-''''''
 ]
 
 # Medium prompts
+medium_prompts = []
 
 # Hard prompts
+hard_prompts = []
