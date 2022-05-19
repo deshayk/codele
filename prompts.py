95623fea6457ec350e6a9761cbf3fcf5f7157315
deshayk <deshaykidd@gmail.com>
2022-05-19 00:05:47 -0500

    add easy programming prompts

--- prompts.py
+++ prompts.py
@@ -6,8 +6,26 @@ and will be generated randomly for the user.
 
 # Easy prompts
 
-easy_prompts = [
-
+easy_prompts = ["Write a program that prints ‘Hello World’ to the screen.",
+"Write a program that asks the user for their name and greets them with their name.",
+"Write a program that asks the user for their age and accurately guess what year they were born.",
+"Write a program that asks the user for a number n and prints the sum of the numbers 1 to n.",
+"Write a program that asks the user for a number n and gives them the possibility to choose between computing the sum and computing the product of 1,…,n.",
+"Write a program that prints a multiplication table for numbers up to 12.",
+"Write a program that prints all prime numbers. (Note: if your programming language does not support arbitrary size numbers, printing all primes up to the largest number you can easily represent is fine too.)",
+"Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. It counts only as one try if they input the same number multiple times consecutively.",
+"Write a program that prints the next 20 leap years.",
+"Write a function that returns the largest element in a list.",
+"Write function that reverses a list, preferably in place.",
+"Write a function that checks whether an element occurs in a list.",
+"Write a function that returns the elements on odd positions in a list.",
+"Write a function that computes the running total of a list.",
+"Write a function that tests whether a string is a palindrome.",
+"Write three functions that compute the sum of the numbers in a list: using a for-loop, a while-loop and recursion. (Subject to availability of these constructs in your language of choice.)",
+"Write a function that concatenates two lists. [a,b,c], [1,2,3] → [a,b,c,1,2,3]",
+"Write a function that combines two lists by alternatingly taking elements, e.g. [a,b,c], [1,2,3] → [a,1,b,2,c,3].",
+"Write a function that takes a number and returns a list of its digits. So for 2342 it should return [2,3,4,2].",
+"Write function that translates a text to Pig Latin and back. English is translated to Pig Latin by taking the first letter of every word, moving it to the end of the word and adding ‘ay’. “The quick brown fox” becomes “Hetay uickqay rownbay oxfay”."            
 ]
 
 # Medium prompts
