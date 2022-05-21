f77cbe1671f72362ab9a00240bc2f0d765f321c3
deshayk <deshaykidd@gmail.com>
2022-05-20 23:54:19 -0500

    add welcome class

--- welcome.py
+++ welcome.py
@@ -1,3 +1,5 @@
 '''
 This module welcomes the user to the game and asks them to choose a difficulty level.
 '''
+class Welcome():
+  print("Hello, my name is Nano, and I am your personal assistant. I will be your guide to the world of Programming.")
