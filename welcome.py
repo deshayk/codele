d8873d261f71d3235cdf44dd9b06da7aa452a7cb
deshayk <deshaykidd@gmail.com>
2022-05-21 23:56:09 -0500

    update welcome message

--- welcome.py
+++ welcome.py
@@ -3,3 +3,4 @@ This module welcomes the user to the game and asks them to choose a difficulty l
 '''
 class Welcome():
   print("Hello, my name is Nano, and I am your personal assistant. I will be your guide to the world of Programming.")
+  print("The purpose of this program is to help you become a better developer.")
