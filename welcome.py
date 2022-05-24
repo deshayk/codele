18f7c2b48b9c8b128be18c71c2395786088f7f50
deshayk <deshaykidd@gmail.com>
2022-05-23 22:00:05 -0500

    update comment

--- welcome.py
+++ welcome.py
@@ -1,5 +1,5 @@
 '''
-This module welcomes the user to the game and asks them to choose a difficulty level.
+This module contains the AI's messaging prompts.
 '''
 class Welcome():
   print("Hello, my name is Nano, and I am your personal assistant. I will be your guide to the world of Programming.")
